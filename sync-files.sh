ad337b2343b4b6881d6e01cf4fd63a85925e1c2b
Skimlk <vanhoutencocoapowder@gmail.com>
2025-02-27 17:10:32 -0500

    Change terminal font size, check for changes before copying

--- sync-files.sh
+++ sync-files.sh
@@ -8,11 +8,11 @@ USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
 
 # Define files array with format: "{Filename} {Application Dir} {Dotfiles Dir}"
 files=(
-	"i3status.conf		/etc/							./i3/"
-	"config 			$USER_HOME/.config/i3/			./i3/"
-	".bashrc 			$USER_HOME/						./"
-	".vimrc				$USER_HOME/						./"
-	".xbindkeysrc		$USER_HOME/						./"
+	"i3status.conf		/etc/				./i3/"
+	"config 		$USER_HOME/.config/i3/		./i3/"
+	".bashrc 		$USER_HOME/			./"
+	".vimrc			$USER_HOME/			./"
+	".xbindkeysrc		$USER_HOME/			./"
 	"lxterminal.conf	$USER_HOME/.config/lxterminal/	./"
 )
 
@@ -29,17 +29,25 @@ push() {
 	dest=$(echo "$file" | awk '{print $2}')
 }
 
+copy_files() {
+	if cp $src $dest; then
+		echo "Copied '$src' to '$dest'"
+	else
+		echo "Failed to copy '$src' to '$dest'"
+	fi
+}
+
 if [ -n "$1" ] && declare -f "$1" > /dev/null; then
 	for file in "${files[@]}"; do
 		$1
-		if cp $src $dest; then
-			echo "Copied '$src' to '$dest'"
+		if [ -n "$(diff $src $dest)" ]; then
+			copy_files
 		else
-			echo "Failed to copy '$src' to '$dest'"
+			echo "No change to '$src'"
 		fi
 	done
 else
 	echo "Error: Incorrect syntax or invalid function."
 	echo "Usage: $0 {pull|push}"
-	exit 1	
+	exit 1
 fi
