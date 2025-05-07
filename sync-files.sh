2bb2c3c92b3eea97818102f8ad5aa79d9eceec69
Skimlk <vanhoutencocoapowder@gmail.com>
2025-05-06 23:04:06 -0400

    Allow for operation on specified programs

--- sync-files.sh
+++ sync-files.sh
@@ -6,27 +6,27 @@ cd "$script_path" || exit 1
 
 USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
 
-# Define files array with format: "{Filename} {Application Dir} {Dotfiles Dir}"
+# Define files array with format: "{Application Name} {Filename} {Application Dir} {Dotfiles Dir}"
 files=(
-	"i3status.conf		/etc/				./i3/"
-	"config 		$USER_HOME/.config/i3/		./i3/"
-	".bashrc 		$USER_HOME/			./"
-	".vimrc			$USER_HOME/			./"
-	".xbindkeysrc		$USER_HOME/			./"
-	"lxterminal.conf	$USER_HOME/.config/lxterminal/	./"
+	"i3		i3status.conf		/etc/				./i3/"
+	"i3		config 		$USER_HOME/.config/i3/		./i3/"
+	"bash		.bashrc 		$USER_HOME/			./"
+	"vim		.vimrc			$USER_HOME/			./"
+	"xbindkeys	.xbindkeysrc		$USER_HOME/			./"
+	"lxterminal	lxterminal.conf		$USER_HOME/.config/lxterminal/	./"
 )
 
 pull() {
-	src=$(echo "$file" | awk '{print $2 $1}')
-	dest=$(echo "$file" | awk '{print $3}')
+	src=$(echo "$file" | awk '{print $3 $2}')
+	dest=$(echo "$file" | awk '{print $4}')
 }
 push() {
 	if [ "$EUID" -ne 0 ]; then
 		echo "Error: You must have root privileges to push files."
 		exit 1
 	fi
-	src=$(echo "$file" | awk '{print $3 $1}')
-	dest=$(echo "$file" | awk '{print $2}')
+	src=$(echo "$file" | awk '{print $4 $2}')
+	dest=$(echo "$file" | awk '{print $3}')
 }
 
 copy_files() {
@@ -37,17 +37,34 @@ copy_files() {
 	fi
 }
 
-if [ -n "$1" ] && declare -f "$1" > /dev/null; then
+operate() {
+	$operation
+	if [ -n "$(diff $src $dest)" ]; then
+		copy_files
+	else
+		echo "No change to '$src'"
+	fi
+}
+
+operation=$1
+if [ -n "$operation" ] && declare -f "$operation" > /dev/null; then
+	if [ -n "$2" ]; then
+		arg_files=()
+		for arg in "${@:2}"; do 
+			for line in "${files[@]}"; do
+				if [ $(echo $line | grep -Eo '^[^ ]+') == $arg ]; then
+					arg_files+=("$line")
+				fi
+			done
+		done
+		files=("${arg_files[@]}")
+	fi
+
 	for file in "${files[@]}"; do
-		$1
-		if [ -n "$(diff $src $dest)" ]; then
-			copy_files
-		else
-			echo "No change to '$src'"
-		fi
+		operate
 	done
 else
 	echo "Error: Incorrect syntax or invalid function."
-	echo "Usage: $0 {pull|push}"
+	echo "Usage: $0 {pull|push} {application}"
 	exit 1
 fi
