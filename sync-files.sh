b1aa117c7304a5bab114f2920dcac0bd16251bea
Skim <vanhoutencocoapowder@gmail.com>
2026-04-08 17:05:27 -0400

    Clean up whitespace in sync-files.sh

--- sync-files.sh
+++ sync-files.sh
@@ -8,11 +8,11 @@ USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
 
 # Define files array with format: "{Application Name} {Filename} {Application Dir} {Dotfiles Dir}"
 files=(
-	"i3		i3status.conf		/etc/				./i3/"
-	"i3		config 		$USER_HOME/.config/i3/		./i3/"
-	"bash		.bashrc 		$USER_HOME/			./"
-	"vim		.vimrc			$USER_HOME/			./"
-	"xbindkeys	.xbindkeysrc		$USER_HOME/			./"
+	"i3			i3status.conf		/etc/					.		./i3/"
+	"i3			config 				$USER_HOME/.config/i3/			./i3/"
+	"bash		.bashrc 			$USER_HOME/						./"
+	"vim		.vimrc				$USER_HOME/						./"
+	"xbindkeys	.xbindkeysrc		$USER_HOME/						./"
 	"lxterminal	lxterminal.conf		$USER_HOME/.config/lxterminal/	./"
 )
 
@@ -20,6 +20,7 @@ pull() {
 	src=$(echo "$file" | awk '{print $3 $2}')
 	dest=$(echo "$file" | awk '{print $4}')
 }
+
 push() {
 	if [ "$EUID" -ne 0 ]; then
 		echo "Error: You must have root privileges to push files."
